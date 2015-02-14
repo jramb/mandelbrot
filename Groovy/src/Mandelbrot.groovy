e31d9c82c655339d6442ca49f987dbef6fad5cbf
Jörg Ramb <jorg@jramb.com>
2015-02-14 23:10:35 +0100

    reworked Groovy (still slow as hell)

---
+++ Groovy/src/Mandelbrot.groovy
@@ -0,0 +1,41 @@
+#!/usr/bin/env groovy
+// 2014, hacked together with NO good knowledge of Groovy, really
+//println "Hello from the shebang line"
+
+int depth(double cx, double cy, int max) {
+    double x2, y2;
+    double zx = cx;
+    double zy = cy;
+    int i = -1;
+    while (++i <= max && (x2 = zx * zx) + (y2 = zy * zy) < 4.0) {
+      zy = zx * zy * 2.0 + cy;
+      zx = x2 - y2 + cx;
+    }
+    return i;
+}
+
+//assert 6 == depth(0.5, 0.5, 100);
+
+
+char depth2Char(int d) {
+  return (char) (((int)'a') + (d % 26));
+}
+
+void mandelbrot(int w, int h, int max) {
+    double stepY = 2.0 / h, step1 = 3.0 / w
+    for (y = -1.0; y < 1.0; y += stepY) {
+      sb = "";
+      for (x = -2.0; x < 1.0; x += step1) {
+        d = depth(x,y,max);
+        sb += depth(x, y, max) > max ? ' ' : depth2Char(d);
+      }
+      println sb;
+    }
+}
+
+w = Integer.parseInt args[0];
+h = Integer.parseInt args[1];
+max = Integer.parseInt args[2];
+mandelbrot(w,h, max as int)
+println args
+
