186af05c636de0513e32377cbefbd22fcff27594
Jörg Ramb <jorg@jramb.com>
2015-04-18 22:46:46 +0200

    fixed a stupid bug (performance improved somewhat)

--- Groovy/src/Mandelbrot.groovy
+++ Groovy/src/Mandelbrot.groovy
@@ -3,31 +3,31 @@
 //println "Hello from the shebang line"
 
 int depth(double cx, double cy, int max) {
-    double x2, y2;
-    double zx = cx;
-    double zy = cy;
-    int i = -1;
+    double x2, y2
+    double zx = cx
+    double zy = cy
+    int i = -1
     while (++i <= max && (x2 = zx * zx) + (y2 = zy * zy) < 4.0) {
-      zy = zx * zy * 2.0 + cy;
-      zx = x2 - y2 + cx;
+      zy = zx * zy * 2.0 + cy
+      zx = x2 - y2 + cx
     }
-    return i;
+    i
 }
 
 //assert 6 == depth(0.5, 0.5, 100);
 
 
 char depth2Char(int d) {
-  return (char) (((int)'a') + (d % 26));
+  (char) (((int)'a') + (d % 26));
 }
 
-void mandelbrot(int w, int h, int max) {
+def mandelbrot(int w, int h, int max) {
     double stepY = 2.0 / h, step1 = 3.0 / w
     for (y = -1.0; y < 1.0; y += stepY) {
       sb = "";
       for (x = -2.0; x < 1.0; x += step1) {
-        d = depth(x,y,max);
-        sb += depth(x, y, max) > max ? ' ' : depth2Char(d);
+        d = depth x,y,max
+        sb += d > max ? ' ' : depth2Char(d)
       }
       println sb;
     }
